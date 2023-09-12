2a22dfedb6b16b049207c4bbb9b317eaada583c1
Yafet Tedla <65037439+Fettii@users.noreply.github.com>
2023-09-12 11:30:57 -0400

    .

--- LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignment2/WordCounting.java
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignment2/WordCounting.java
@@ -1,9 +1,31 @@
 package org.howard.edu.lsp.assignment2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class WordCounting {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String folderPath = "Users/drtun/git/LSP_GIT_YT_FALL2023/LSP_Assignment_Fall_2023/src/main/java/resources/";
+		String fileName = "words.txt";
+		
+		String filePath = folderPath+fileName;
+		try (FileReader fileReader = new FileReader(filePath);
+	             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+			
+			String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                // Process each line here (e.g., print it)
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+			
+			
+		
+		
 
 	}
 
