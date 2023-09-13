4df455a7f851b69a95b822c9086ec3b106a7ff7d
Yafet Tedla <65037439+Fettii@users.noreply.github.com>
2023-09-12 21:58:34 -0400

    .

--- LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignment2/WordCounting.java
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignment2/WordCounting.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class WordCounting {
 
 	public static void main(String[] args) {
-		String folderPath = "Users/drtun/git/LSP_GIT_YT_FALL2023/LSP_Assignment_Fall_2023/src/main/java/resources/";
+		String folderPath = "C:\\Users\\drtun\\git\\LSP_GIT_YT_FALL2023\\LSP_Assignment_Fall_2023\\src\\main\\java\\resources\\";
 		String fileName = "words.txt";
 		
 		String filePath = folderPath+fileName;
