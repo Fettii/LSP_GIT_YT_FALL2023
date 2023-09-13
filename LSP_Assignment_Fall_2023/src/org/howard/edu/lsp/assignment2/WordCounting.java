79df2b31c00a5786aafce5a248f7570283c2ab3c
Yafet Tedla <65037439+Fettii@users.noreply.github.com>
2023-09-12 22:50:56 -0400

    .

--- LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignment2/WordCounting.java
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignment2/WordCounting.java
@@ -3,12 +3,15 @@ package org.howard.edu.lsp.assignment2;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WordCounting {
 
 	public static void main(String[] args) {
 		String folderPath = "C:\\Users\\drtun\\git\\LSP_GIT_YT_FALL2023\\LSP_Assignment_Fall_2023\\src\\main\\java\\resources\\";
 		String fileName = "words.txt";
+		 Map<String,Integer> wordCounts = new HashMap<>();
 		
 		String filePath = folderPath+fileName;
 		try (FileReader fileReader = new FileReader(filePath);
@@ -16,12 +19,16 @@ public class WordCounting {
 			
 			String line;
             while ((line = bufferedReader.readLine()) != null) {
-                // Process each line here (e.g., print it)
-                System.out.println(line);
+            	line = line.toLowerCase();
+            	wordCounts.put(line, wordCounts.getOrDefault(line, 0) + 1);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+		
+		for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
+		    System.out.println(entry.getKey() + ": " + entry.getValue());
+		}
 			
 			
 		
