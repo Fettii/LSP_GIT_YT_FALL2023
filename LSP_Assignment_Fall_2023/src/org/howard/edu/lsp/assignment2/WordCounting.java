eadb00566be2051610fa94655bc029f83df5f1b4
Yafet Tedla <65037439+Fettii@users.noreply.github.com>
2023-09-18 20:56:15 -0400

    .

--- LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignment2/WordCounting.java
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignment2/WordCounting.java
@@ -4,36 +4,52 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 public class WordCounting {
 
-	public static void main(String[] args) {
-		String folderPath = "C:\\Users\\drtun\\git\\LSP_GIT_YT_FALL2023\\LSP_Assignment_Fall_2023\\src\\main\\java\\resources\\";
-		String fileName = "words.txt";
-		 Map<String,Integer> wordCounts = new HashMap<>();
-		
-		String filePath = folderPath+fileName;
-		try (FileReader fileReader = new FileReader(filePath);
-	             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
-			
-			String line;
+    public static void main(String[] args) {
+        String folderPath = "C:\\Users\\drtun\\git\\LSP_GIT_YT_FALL2023\\LSP_Assignment_Fall_2023\\src\\main\\java\\resources\\";
+        String fileName = "words.txt";
+        HashMap<String, Integer> wordCounts = new HashMap<>();
+        String line;
+        
+
+        
+        String filePath = folderPath + fileName;
+
+        try (FileReader fileReader = new FileReader(filePath);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+            
             while ((line = bufferedReader.readLine()) != null) {
-            	line = line.toLowerCase();
-            	wordCounts.put(line, wordCounts.getOrDefault(line, 0) + 1);
+                
+                line = line.toLowerCase();
+                String[] words = line.split(" ");
+                
+                for (String word : words) {
+                    
+                    if (wordCounts.containsKey(word)) {
+                        int count = wordCounts.get(word);
+                        wordCounts.put(word, count + 1);
+                    } else {
+                        
+                        wordCounts.put(word, 1);
+                    }
+                }
+                
+               
             }
+
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
-		
-		for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
-		    System.out.println(entry.getKey() + ": " + entry.getValue());
-		}
-			
-			
-		
-		
-
-	}
-
+        for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
+            String word = entry.getKey();
+            int count = entry.getValue();
+            System.out.println(word + ": " + count);
+        }
+    }
 }
