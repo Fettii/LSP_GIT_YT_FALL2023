4aacc05e144f398675f1bf0deafdc1de7a0982ba
Yafet Tedla <65037439+Fettii@users.noreply.github.com>
2023-10-14 22:47:52 -0400

    first committ assignment 4

---
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignment4/IntegerSet.java
@@ -0,0 +1,101 @@
+package org.howard.edu.lsp.assignment4;
+
+public class IntegerSet {
+	
+// Hint: probably best to use an array list.  You will need to do a little research
+	private List<Integer> set = new ArrayList<Integer>();
+
+	// Default Constructor
+	public IntegerSet() {
+	}
+	
+	public IntegerSet(ArrayList<Integer> set) {
+		this.set = set;
+	}
+	
+	// Clears the internal representation of the set
+	public void clear() {
+		
+	}
+	
+	// Returns the length of the set
+	public int length() {
+		
+	}
+	
+	public boolean equals(Object o) {
+		
+	}
+	
+	// Returns true if the set contains the value, otherwise false
+	public boolean contains(int value) {
+		
+	}
+	
+	// Returns the largest item in the set; Throws a IntegerSetException if the set is empty 
+	public int largest() {
+		
+	}
+	
+	// Returns the smallest item in the set; Throws a IntegerSetException if the set is empty
+	public int smallest() {
+		
+	}
+	
+	// Adds an item to the set or does nothing it already there	
+	public void add(int item) {
+		
+	}
+	
+	// Removes an item from the set or does nothing if not there
+	public void remove(int item) {
+		
+	}
+	
+	// Set union
+	public void union(IntegerSet intSetb) {
+		
+	}
+	
+	// Set intersection, all elements in s1 and s2
+	public void intersect(IntegerSet intSetb) {
+		
+	}
+	
+	// Set difference, i.e., s1 –s2
+	public void diff(IntegerSet intSetb) {
+		
+	}; 
+	
+	// Set complement, all elements not in s1
+	public void complement(IntegerSet intSetb) {
+	};
+	
+	// Returns true if the set is empty, false otherwise
+	boolean isEmpty() {
+		
+	}; 
+	
+	// Return String representation of your set
+	public String toString() {
+		
+	};
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+	
+	
+
+}
